ed28912e8e5846f422960529b7c2dbb10d1d1005
johan windows <140156@unsaac.edu.pe>
2021-08-25 22:24:59 -0500

    inicio

---
+++ transport_path_planning/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for transport_path_planning project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transport_path_planning.settings')
+
+application = get_asgi_application()
---
+++ transport_path_planning/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for transport_path_planning project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transport_path_planning.settings')
+
+application = get_wsgi_application()
