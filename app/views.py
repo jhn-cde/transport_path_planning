10705dd8786b1960b7c709ce31762e25ed02c1af
johan windows <140156@unsaac.edu.pe>
2021-09-16 08:54:13 -0500

    se añadio class (seleccionado) a botones 1 2 3

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
+from django.http import request
 from django.shortcuts import render, redirect
 from django.conf import settings
-from .models import Almacen, AlmacenPoint, Tienda, TiendaPoint, PolygonLayer
+from .models import AlmacenPoint, TiendaPoint, PolygonLayer
 from .mixins import getlatlng, guardarObjeto, getDictCoords
 from .utils.voronoi import DiagramaVoronoi
 from .utils.DistanciaMinima import MinDistancia
@@ -19,7 +20,7 @@ def tiendas(request):
         latlng = getlatlng(address, settings.GOOGLE_API_KEY)
         # añadir nombre
         if (latlng["address"] == ""):
-            latlng["address"] = "Tienda " + str(len(Tienda.objects.all()))
+            latlng["address"] = "Tienda " + str(len(TiendaPoint.objects.all()))
         # guardar punto            
         tiendaPunto = TiendaPoint()
         guardarObjeto(tiendaPunto, latlng, "Tienda")
@@ -36,7 +37,7 @@ def tiendas(request):
     return render(request, 'app/tiendas.html', context)
 
 def deltiendas(request):
-    Tienda.objects.all().delete()
+    TiendaPoint.objects.all().delete()
     return tiendas(request)
 
 def almacenes(request):
@@ -47,7 +48,7 @@ def almacenes(request):
         latlng = getlatlng(address, settings.GOOGLE_API_KEY)
         # añadir nombre
         if (latlng["address"] == ""):
-            latlng["address"] = "Almacen " + str(len(Almacen.objects.all()))
+            latlng["address"] = "Almacen " + str(len(AlmacenPoint.objects.all()))
         # guardar punto
         almacenPunto = AlmacenPoint()
         guardarObjeto(almacenPunto, latlng, "Almacen")
@@ -64,12 +65,14 @@ def almacenes(request):
     return render(request, 'app/almacenes.html', context)
 
 def delalmacenes(request):
-    Almacen.objects.all().delete()
+    AlmacenPoint.objects.all().delete()
     return almacenes(request)
 
 def resultado(request):
+    # Eliminar poligonos anteriores
+    PolygonLayer.objects.all().delete()
+
     # obtener modelos serializados
-    ser = GeoJSONSerializer().serialize(PolygonLayer.objects.all(), use_natural_keys=False, with_modelname=False)
     almSer = GeoJSONSerializer().serialize(AlmacenPoint.objects.all(), use_natural_keys=False, with_modelname=False)
     tieSer = GeoJSONSerializer().serialize(TiendaPoint.objects.all(), use_natural_keys=False, with_modelname=False)
 
@@ -82,9 +85,6 @@ def resultado(request):
     diagramas_voronoi = DiagramaVoronoi(d_almSer)
     print("* Diagramas de Voronoi Generados")
 
-    # Eliminar poligonos anteriores
-    PolygonLayer.objects.all().delete()
-
     # generar nuevos poligonos
     print("\n* Generando poligonos - regiones")
     regiones = diagramas_voronoi.regiones()
@@ -93,6 +93,9 @@ def resultado(request):
         pol.geom = v
         pol.save()
     print("* Poligonos generados")
+
+    # obtener poligonos serializados
+    ser = GeoJSONSerializer().serialize(PolygonLayer.objects.all(), use_natural_keys=False, with_modelname=False)
 
     print("\n* Generando rutas optimas")
     # Generar rutas
@@ -126,3 +129,9 @@ def resultado(request):
         "rutasSer": rutasSer
     }
     return render(request, "app/resultado.html", context)
+
+def delresultado(request):
+    TiendaPoint.objects.all().delete()
+    AlmacenPoint.objects.all().delete()
+    PolygonLayer.objects.all().delete()
+    return almacenes(request)
--- transport_path_planning/settings.py
+++ transport_path_planning/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-    'crispy_forms',
     'djgeojson',
 ]
 
